d1a2588f1ac0d5eb8c3ff960dd14a05c91b32e53
gaochao <18853503922@163.com>
2017-12-14 15:55:11 +0800

    _partition_manager can use,but code should  optimize！！！

--- backup.py
+++ backup.py
@@ -16,11 +16,12 @@ backup_dirname =  "%s/%s" % (backup_path , dirname)
 xtrabackup_dir = "%s/xtrabackup" % backup_dirname
 mysqldump_dir = "%s/mysqldump" % backup_dirname
 
-if os.path.exists("%s" % backup_dirname):
-    pass
-else:
-    print 'create backup directory: %s' % backup_dirname
-    os.makedirs('%s' % backup_dirname) 
+def init_directory():
+    if os.path.exists("%s" % backup_dirname):
+        pass
+    else:
+        print 'create backup directory: %s' % backup_dirname
+        os.makedirs('%s' % backup_dirname) 
 
 
 ### define log and wrapper
@@ -102,36 +103,79 @@ def _mysqldump():
         cmd = "/srv/mysql3306/bin/mysqldump --host={host} --port={port} --user={user} --password={password} --single-transaction --master-data=2 %s > %s/%s.sql".format(**db_config) % (db,mysqldump_dir,db)
         os.system(cmd)
 ### partition manager
+'''
+!!!you need to create manage database and tables
+create database mulberry_test;
+
+CREATE TABLE `mb_table_config` (
+`table_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+`schema_name` varchar(64) NOT NULL COMMENT '数据库名',
+`table_name` varchar(128) NOT NULL COMMENT '表名',
+`forward_day` int(11) NOT NULL COMMENT '从今天算，提前创建的天数',
+`clear_before_day` int(11) NOT NULL COMMENT '从当天算，清除之前的天数',
+`create_time` bigint(20) NOT NULL,
+`update_time` bigint(20) DEFAULT '0',
+PRIMARY KEY (`table_id`),
+UNIQUE KEY `schema_name_table_name` (`schema_name`,`table_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT="分区配置信息表";
+
+CREATE TABLE `patition_manager_log` (
+`id` int(11) unsigned AUTO_INCREMENT COMMENT '主键',
+`schema_name` varchar(64) NOT NULL COMMENT '数据库名',
+`table_name` varchar(128) NOT NULL COMMENT '表名',
+`add_partition_name` varchar(64) NOT NULL COMMENT '增加的分区名',
+`status` tinyint(4) NOT NULL COMMENT '状态:1-->成功,0-->失败',
+`sql` varchar(256) NOT NULL NULL DEFAULT '' COMMENT '执行的SQL',
+`description` varchar(1024) NOT NULL NULL DEFAULT '' COMMENT '错误信息',
+`create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+`update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='分区日志表';
+
+
+'''
 def _partition_manager():
-    import time,datetime
+    import sys,time,datetime
     cnx = mysql.connector.connect(**db_config)
     cursor = cnx.cursor()
     taday = time.strftime("%Y-%m-%d", time.localtime()) + " 00:00:00" 
 
     #获得配置表分区信息
-    query_config_table_info = (""" select * from mulberry_test.mb_table_config order by table_id; """)
-    cursor.execute(query_config_table_info)
-    query_config_table_info_results = cursor.fetchall()
+    query_config_table_info = (""" select schema_name,table_name from mulberry_test.mb_table_config order by table_id; """)
+    try:
+        cursor.execute(query_config_table_info)
+        query_config_table_info_results = cursor.fetchall()
+    except Exception,ex:
+        print Exception,":",ex 
+        sys.exit()
     for r in query_config_table_info_results:
-        Schema_name = r[1]
-        Table_name = r[2]
+        Schema_name = r[0]
+        Table_name = r[1]
         #获得服器分区表信息
-        #query_server_partition_info = (""" select table_schema,table_name,partition_name,partition_description from information_schema.partitions where partition_name is not null; """)  
         query_server_partition_info = (""" select table_schema,table_name from information_schema.partitions where partition_name is not null group by table_schema,table_name; """)  
-        cursor.execute(query_server_partition_info)
-        query_server_partition_info_results = cursor.fetchall()
+        try:
+            cursor.execute(query_server_partition_info)
+            query_server_partition_info_results = cursor.fetchall()
+        except Exception,ex:
+            print Exception,":",ex
+            sys.exit()
         #如果服务器中的库名，表名与配置表中的库名，表名一致则进入清理逻辑
         for r in query_server_partition_info_results:           
             if r[0]==Schema_name and r[1]==Table_name:
                # 获得配置表需要分的分区信息
-               query_config_table_need_drop_info = (""" select * from mulberry_test.mb_table_config where schema_name="%s" and table_name="%s" order by table_id; """) %(Schema_name,Table_name)
-               cursor.execute(query_config_table_need_drop_info)
-               query_config_table_need_drop_info_results = cursor.fetchall()
+               query_config_table_need_drop_info = (""" select schema_name,table_name,forward_day,clear_before_day from mulberry_test.mb_table_config 
+                                                    where schema_name="%s" and table_name="%s" order by table_id; """) %(Schema_name,Table_name)
+               try:
+                   cursor.execute(query_config_table_need_drop_info)
+                   query_config_table_need_drop_info_results = cursor.fetchall()
+               except Exception,ex:
+                   print Exception,":",ex
+                   sys.exit()
                for r in query_config_table_need_drop_info_results:
-                   schema_name = r[1]
-                   table_name = r[2]
-                   forward_day = r[3]
-                   clear_before_day = r[4] 
+                   schema_name = r[0]
+                   table_name = r[1]
+                   forward_day = r[2]
+                   clear_before_day = r[3] 
                    #print "%s.%s" %(schema_name,table_name)  #需要处理的库名，表名
 
                    # 获得指库表需要清理的分区
@@ -139,53 +183,67 @@ def _partition_manager():
                                                 where partition_name is not null and table_schema='%s' and table_name='%s' and partition_description != "maxvalue"
                                                 and partition_description<= unix_timestamp(date_sub("%s",interval %s day))*1000
                                                 order by table_schema,table_name,partition_ordinal_position asc;""") % (schema_name,table_name,taday,clear_before_day)
-                   cursor.execute(query_server_clear_before_day_partition)
-                   query_server_clear_before_day_partition_results = cursor.fetchall()
+                   try:
+                       cursor.execute(query_server_clear_before_day_partition)
+                       query_server_clear_before_day_partition_results = cursor.fetchall()
+                   except Exception,ex:
+                       print Exception,":",ex
+                       sys.exit()
                    # drop clear before day partition
                    for partition in query_server_clear_before_day_partition_results:
                        if partition:
                            partition_name = partition[0]
                            query_drop_partition_sql = ("alter table %s.%s drop partition %s;") % (schema_name,table_name,partition_name)
-                           print query_drop_partition_sql
-                           #cursor.execute(query_drop_partition_name)
+                           try:
+                               cursor.execute(query_drop_partition_sql)
+                               print "The %s.%s partition %s is droped,the sql:<%s>" %(schema_name,table_name,partition_name,query_drop_partition_sql)
+                           except Exception,ex:
+                               print Exception,":",ex
                        else:
                            pass
                    # drop max partition :if max partition is null then drop max partition else not drop max partition
                    query_max_partition_sql = ("""select partition_name from information_schema.partitions 
                                          where table_schema='%s' and table_name='%s' and partition_description = "maxvalue";""") %(schema_name,table_name)
-                   cursor.execute(query_max_partition_sql)
-                   query_max_partition_sql_results = cursor.fetchall()
+                   try:
+                       cursor.execute(query_max_partition_sql)
+                       query_max_partition_sql_results = cursor.fetchall()
+                   except Exception,ex:
+                       print Exception,":",ex
                    for partition in query_max_partition_sql_results:
                        if partition:
                            max_partition_name = partition[0]
                            query_not_empty = ("""select 1 from %s.%s partition(%s) limit 1;""") % (schema_name,table_name,max_partition_name)
-                           cursor.execute(query_not_empty)
-                           query_not_empty_results = cursor.fetchall()
+                           try:
+                               cursor.execute(query_not_empty)
+                               query_not_empty_results = cursor.fetchall()
+                           except Exception,ex:
+                               print Exception,":",ex
                            if query_not_empty_results:
                                print "The %s.%s partition %s is not empty" %(schema_name,table_name,max_partition_name)
                            else:
-                               query_drop_max_partition_sql = ("""alter table %s.%s drop partition %s; """) % (schema_name,table_name,max_partition_name)
-                               print query_drop_max_partition_sql 
-                               #cursor.execute(query_drop_max_partition)
+                               query_drop_max_partition_sql = ("""alter table %s.%s drop partition %s;""") % (schema_name,table_name,max_partition_name)
+                               try:
+                                   cursor.execute(query_drop_max_partition_sql)
+                                   print "The %s.%s partition %s is droped,the sql: <%s>" %(schema_name,table_name,max_partition_name,query_drop_max_partition_sql)  
+                               except Exception,ex:
+                                   print Exception,":",ex
                        else:
                            pass
                    # get exist forward day partition
-                  # query_server_forward_day_partition = ("""select partition_description from information_schema.partitions
-                  #                              where partition_name is not null and table_schema='%s' and table_name='%s' and partition_description != "maxvalue"
-                  #                              and partition_description> unix_timestamp(date_sub("%s",interval -%s day))*1000
-                  #                              order by table_schema,table_name,partition_ordinal_position asc;""") % (schema_name,table_name,taday,forward_day) 
                    query_server_forward_day_partition_sql = ("""select partition_description from information_schema.partitions
                                                 where partition_name is not null and table_schema='%s' and table_name='%s' and partition_description != "maxvalue"
                                                 order by table_schema,table_name,partition_ordinal_position asc;""") % (schema_name,table_name) 
-                   cursor.execute(query_server_forward_day_partition_sql)
-                   query_server_forward_day_partition_sql_results = cursor.fetchall() 
-                   #print query_server_forward_day_partition_sql_results
+                   try:
+                       cursor.execute(query_server_forward_day_partition_sql)
+                       query_server_forward_day_partition_sql_results = cursor.fetchall() 
+                   except Exception,ex:
+                        print Exception,":",ex
+                   # 循环需要向后添加的天数，判断是否需要添加，如果分区已经存在则不处理，如果不存在则添加指定的分区
                    for i in range(forward_day):
                        now = datetime.datetime.now()
                        s=now.strftime('%Y-%m-%d')
                        d = datetime.datetime.strptime(s,"%Y-%m-%d")
-                       partition_description =  int(time.mktime(d.timetuple()) + 3600*24*i)*1000
-                       #print "doday partition_description is %s" % partition_description
+                       partition_description = int(time.mktime(d.timetuple()) + 3600*24*i)*1000
                        suffix_time = time.strftime("%Y%m%d",time.localtime((partition_description -3600*24)/1000))
                        add_patition_name = "p_auto_new_%s" % suffix_time
                        if len(query_server_forward_day_partition_sql_results) == 0:
@@ -195,26 +253,14 @@ def _partition_manager():
                            for description in query_server_forward_day_partition_sql_results:
                                change_to_list.append(description[0])
                            if "%s" % partition_description in change_to_list:
-                                print partition_description
+                                print "The %s.%s partition name %s is exist, the partition_description is %s" %(schema_name,table_name,add_patition_name,partition_description)
                            else:
-                                print "need add %s"  % description
-                           #        print "The exist description=%s and the today description=%s is exist" % (description[0],partition_description)
-                           #    else:
-                           #        print "The need add partition is %s" %description[0]
-#                              # print "i=%s schema_name=%s" %(i,schema_name)
-#                              # if len(query_server_forward_day_partition_results) != 0:
-#                              #     query_add_partiiton = (""" alter table %s.%s add partition(partition %s values less than(%s));""") % (schema_name,table_name,add_patition_name,partition_description)
-#                              #     print query_add_partiiton
-#                           #else:
-#                           #    for description in query_server_forward_day_partition_results:
-#                           #        if description:
-#                           #            print description[0] + 'aa'
-#                           #            query_add_partiiton = (""" alter table %s.%s add partition(partition %s values less than("%s"))""") % (schema_name,table_name,add_patition_name,partition_description)
-#                           #        else:
-#                           #            query_add_partiiton = (""" alter table %s.%s add partition(partition %s values less than("%s"))""") % (schema_name,table_name,add_patition_name,partition_description)
-#                           #            print query_add_partiiton
-#                           #            pass
-#                               
+                                add_paririon_sql = ("""alter table %s.%s add partition(partition %s values less than(%s));""") % (schema_name,table_name,add_patition_name,partition_description)
+                                try:
+                                    cursor.execute(add_paririon_sql)
+                                    print  "The %s.%s partition_name %s add completed, the sql: <%s>" %(schema_name,table_name,add_patition_name,add_paririon_sql)
+                                except Exception,ex:
+                                    print Exception,":",ex
     cursor.close()
     cnx.close()
 
